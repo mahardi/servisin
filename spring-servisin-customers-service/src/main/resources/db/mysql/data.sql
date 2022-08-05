60f0fb94ac21c201798bef33ad6d7641cd65c295
mahardi <prabowomahardi@gmail.com>
2022-08-05 17:28:18 +0700

    update query

--- spring-servisin-customers-service/src/main/resources/db/mysql/data.sql
+++ spring-servisin-customers-service/src/main/resources/db/mysql/data.sql
@@ -1,14 +1,14 @@
-INSERT IGNORE INTO types VALUES (1, 'SUV');
-INSERT IGNORE INTO types VALUES (2, 'Mini Van');
-INSERT IGNORE INTO types VALUES (3, 'Coupe');
-INSERT IGNORE INTO types VALUES (4, 'Sport');
-INSERT IGNORE INTO types VALUES (5, 'Truck');
-INSERT IGNORE INTO types VALUES (6, 'Sedan');
+INSERT INTO types(id, name) VALUES (1, 'SUV');
+INSERT INTO types(id, name) VALUES (2, 'Mini Van');
+INSERT INTO types(id, name) VALUES (3, 'Coupe');
+INSERT INTO types(id, name) VALUES (4, 'Sport');
+INSERT INTO types(id, name) VALUES (5, 'Truck');
+INSERT INTO types(id, name) VALUES (6, 'Sedan');
 
-INSERT IGNORE INTO owners VALUES (1, 'Rama', 'Utama', 'Jl. Poso', 'Jakarta Utara', '621812314');
-INSERT IGNORE INTO owners VALUES (2, 'Romli', 'Anwar', 'Jl. Blitar Raya', 'Jakarta Timur', '628512312');
-INSERT IGNORE INTO owners VALUES (3, 'Bianca', 'Putri', 'Jl. Pulo Ribung', 'Bekasi', '6281431244');
+INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES (1, 'Rama', 'Utama', 'Jl. Poso', 'Jakarta Utara', '621812314');
+INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES (2, 'Romli', 'Anwar', 'Jl. Blitar Raya', 'Jakarta Timur', '628512312');
+INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES (3, 'Bianca', 'Putri', 'Jl. Pulo Ribung', 'Bekasi', '6281431244');
 
-INSERT IGNORE INTO cars VALUES (1, '2010-09-07', 'N1111', 'Jeep Wrangler', 1, 1);
-INSERT IGNORE INTO cars VALUES (2, '2012-08-06', 'B123AAA', 'Suzuki Espass', 2, 2);
-INSERT IGNORE INTO cars VALUES (3, '2011-04-17','B9314RFD', 'Honda Accord', 6, 3);
+INSERT INTO cars(id, date_of_purchase, license_plate, name, type_id, owner_id) VALUES (1, '2010-09-07', 'N1111', 'Jeep Wrangler', 1, 1);
+INSERT INTO cars(id, date_of_purchase, license_plate, name, type_id, owner_id) VALUES (2, '2012-08-06', 'B123AAA', 'Suzuki Espass', 2, 2);
+INSERT INTO cars(id, date_of_purchase, license_plate, name, type_id, owner_id) VALUES (3, '2011-04-17','B9314RFD', 'Honda Accord', 6, 3);
--- spring-servisin-workshops-service/src/main/resources/db/mysql/data.sql
+++ spring-servisin-workshops-service/src/main/resources/db/mysql/data.sql
@@ -1,11 +1,11 @@
-INSERT IGNORE INTO workshops VALUES (1, 'Green Performance');
-INSERT IGNORE INTO workshops VALUES (2, 'Red Wheels');
-INSERT IGNORE INTO workshops VALUES (3, 'Blue Audio');
+INSERT IGNORE INTO workshops(id, name) VALUES (1, 'Green Performance');
+INSERT IGNORE INTO workshops(id, name) VALUES (2, 'Red Wheels');
+INSERT IGNORE INTO workshops(id, name) VALUES (3, 'Blue Audio');
 
-INSERT IGNORE INTO specialties VALUES (1, 'Performance Tuning');
-INSERT IGNORE INTO specialties VALUES (2, 'Spooring and Balancing');
-INSERT IGNORE INTO specialties VALUES (3, 'Audio');
+INSERT IGNORE INTO specialties(id, name) VALUES (1, 'Performance Tuning');
+INSERT IGNORE INTO specialties(id, name) VALUES (2, 'Spooring and Balancing');
+INSERT IGNORE INTO specialties(id, name) VALUES (3, 'Audio');
 
-INSERT IGNORE INTO workshop_specialties VALUES (1, 1);
-INSERT IGNORE INTO workshop_specialties VALUES (2, 2);
-INSERT IGNORE INTO workshop_specialties VALUES (3, 3);
+INSERT IGNORE INTO workshop_specialties(workshop_id, specialty_id) VALUES (1, 1);
+INSERT IGNORE INTO workshop_specialties(workshop_id, specialty_id) VALUES (2, 2);
+INSERT IGNORE INTO workshop_specialties(workshop_id, specialty_id) VALUES (3, 3);
